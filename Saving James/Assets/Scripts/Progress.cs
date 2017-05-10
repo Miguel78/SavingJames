99716993e017c963b9f1dc62d488f2f63e6dd4a0
Natalia Montoya <natalia.montoya.sanchez@gmail.com>
2017-05-09 20:16:59 -0500

    Ganar y perder

--- Saving James/Assets/Scripts/Progress.cs
+++ Saving James/Assets/Scripts/Progress.cs
@@ -2,12 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class Progress : MonoBehaviour {
 
 	public Transform player;
 	public Slider slider;
 	public float length;
+	public GameObject winScreen;
 
 	// Update is called once per frame
 	void Update () {
@@ -16,7 +18,12 @@ public class Progress : MonoBehaviour {
 
 		// Si llegamos al final hacer algo
 		if(slider.value >= 1){
-			
+			winScreen.SetActive (true);
+			Invoke ("Reset", 2f);
 		}
+	}
+
+	void Reset(){
+		SceneManager.LoadScene (0);
 	}
 }
